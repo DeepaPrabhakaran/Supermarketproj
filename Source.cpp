b607191e8a6b75a85d0e3f141686fcb5900d14a7
deepa <deepa.prabhakaran1@gmail.com>
2022-09-11 08:56:14 +0200

    First Commit

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Shopping
+{
+private:
+	int pcode;
+	float price;
+	float dis;
+	string pname;
+public:
+	void menu();
+	void administrator();
+	void buyer();
+	void add();
+	void edit();
+	void remove();
+	void list();
+	void receipt();
+};
+int main()
+{
+	
+	return 0;
+}
